9fa5c4d749b061f6ddb119c9bc7a996636dcdd3c
Harvey Nguyen <nqhuy213@gmail.com>
2022-08-10 20:52:22 +1000

    Fix constant

--- packages/web-app/src/constants/Constants.ts
+++ packages/web-app/src/constants/Constants.ts
@@ -1,10 +1,11 @@
 export class Constants {
-  public static readonly apiUrl = process.env.REACT_APP_API_URL;
-  public static readonly firebaseApiKey = process.env.REACT_APP_FIREBASE_API_KEY;
-  public static readonly firebaseAuthDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
-  public static readonly firebaseProjectId = process.env.REACT_APP_FIREBASE_PROJECT_ID;
-  public static readonly firebaseStorageBucket = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
+  public static readonly apiUrl = process.env.REACT_APP_API_URL as string;
+  public static readonly firebaseApiKey = process.env.REACT_APP_FIREBASE_API_KEY as string;
+  public static readonly firebaseAuthDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN as string;
+  public static readonly firebaseProjectId = process.env.REACT_APP_FIREBASE_PROJECT_ID as string;
+  public static readonly firebaseStorageBucket =
+    process.env.REACT_APP_FIREBASE_STORAGE_BUCKET as string;
   public static readonly firebaseMessagingSenderId =
-    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID;
-  public static readonly firebaseAppId = process.env.REACT_APP_FIREBASE_APP_ID;
+    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID as string;
+  public static readonly firebaseAppId = process.env.REACT_APP_FIREBASE_APP_ID as string;
 }
