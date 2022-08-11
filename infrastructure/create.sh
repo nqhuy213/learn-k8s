7657549b7e3a0f31404614279a5966e42cf0ef36
Harvey Nguyen <nqhuy213@gmail.com>
2022-08-11 11:06:03 +1000

    IAC

---
+++ infrastructure/create.sh
@@ -0,0 +1,5 @@
+aws cloudformation create-stack \
+--template-body file://`pwd`/cloudformation.yml \
+--stack-name harvey-eks-stack \
+--capabilities CAPABILITY_NAMED_IAM \
+--timeout-in-minutes 30
---
+++ infrastructure/deploy.sh
@@ -0,0 +1,1 @@
+aws cloudformation deploy --template-file cloud-formation.yml --stack-name harvey-eks-stack --capabilities CAPABILITY_NAMED_IAM
